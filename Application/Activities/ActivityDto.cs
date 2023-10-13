5dcd13fff689b7ca4ce5adce6c6f472f686d85ad
vuthanhdat <vudat.fami@gmail.com>
2023-10-13 15:05:22 +0700

    Chapter 16 End

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -16,6 +16,6 @@ namespace Application.Activities
         public string City { get; set; }
         public string Venue { get; set; }
         public string HostUsername { get; set; }
-        public ICollection<Profile> Attendees { get; set; }
+        public ICollection<AttendeeDto> Attendees { get; set; }
     }
 }
